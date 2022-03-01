85d266842b862b515dc03b0565cd6900c72aa57d
lore <751029317@qq.com>
2022-03-01 12:03:36 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import List from './components/List'
 import Search from './components/Search'
 export default class App extends Component {
+  state = {
+    users: [],
+  }
+  
   render() {
     return (
       <div>
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './index.css';
 export default class Search extends Component {
     keyWordContainer = React.createRef()
+
+    search = () => {
+        const {value} = this.keyWordContainer.current;
+        if (!value.trim()) return alert("输入不能为空");
+        axios.get(`https://api.github.com/search/users?q=${value}`).then(
+            response => {console.log("success", response.data.items)},
+            error => {console.log('failed', error)},
+        )
+    }
+
     render() {
         return (
             <section className="jumbotron">
                 <h3 className="jumbotron-heading">github用户搜索</h3>
                 <div>
                     <input type="text" ref={this.keyWordContainer} placeholder="请输入搜索关键词" />&nbsp;
-                    <button>搜索</button>
+                    <button onClick={this.search}>搜索</button>
                 </div>
             </section>
         )
