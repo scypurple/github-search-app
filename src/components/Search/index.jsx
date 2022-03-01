85f4d3ed1270fafebdbbb898d442e5a99cf62cb9
lore <751029317@qq.com>
2022-03-01 17:21:28 +0800

    fetch

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import PubSub from 'pubsub-js'
 import './index.css';
 
 export default class Search extends Component {
     keyWordContainer = React.createRef()
 
-    search = () => {
-        const {value} = this.keyWordContainer.current;
+    search = async () => {
+        const { value } = this.keyWordContainer.current;
         if (!value.trim()) return alert("输入不能为空");
-        PubSub.publish("updateState" ,{isFirst: false, isLoading: true})
-        axios.get(`https://api.github.com/search/users?q=${value}`).then(
-            response => {
-               PubSub.publish("updateState", {users:response.data.items, isLoading: false})},
-            
-            error => { PubSub.publish("updateState", {error: error.message, isLoading: false})},
-        )
+        PubSub.publish("updateState", { isFirst: false, isLoading: true })
+        // axios.get(`https://api.github.com/search/users?q=${value}`).then(
+        //     response => {
+        //    PubSub.publish("updateState", {users:response.data.items, isLoading: false})},
+
+        // error => { PubSub.publish("updateState", {error: error.message, isLoading: false})},
+        // )
+        // fetch(`https://api.github.com/search/users?q=${value}`).then(
+        //     response => {
+        //         return response.json()
+        //     }).then(
+        //     response => {
+        // PubSub.publish("updateState", {users:response.items, isLoading: false})
+        //     })
+        try {
+            const response = await fetch(`https://api.github.com/search/users?q=${value}`);
+            const data = await response.json();
+            //console.log(data.items);
+            PubSub.publish("updateState", { users: data.items, isLoading: false })
+        } catch (error) {
+            // error =>  PubSub.publish("updateState", {error: error.message, isLoading: false})
+            PubSub.publish("updateState", { error: error.message, isLoading: false })
+        }
     }
 
     handleEnter = (e) => {
         if (e.keyCode === 13) {
-           this.search();
+            this.search();
         }
     }
 
@@ -29,10 +45,10 @@ export default class Search extends Component {
             <section className="jumbotron">
                 <h3 className="jumbotron-heading">github用户搜索</h3>
                 <div>
-                    <input type="text" 
-                    ref={this.keyWordContainer} 
-                    placeholder="请输入搜索关键词" 
-                    onKeyUp={this.handleEnter}/>&nbsp;
+                    <input type="text"
+                        ref={this.keyWordContainer}
+                        placeholder="请输入搜索关键词"
+                        onKeyUp={this.handleEnter} />&nbsp;
                     <button onClick={this.search}>搜索</button>
                 </div>
             </section>
