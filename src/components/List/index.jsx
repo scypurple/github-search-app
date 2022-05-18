0aee4f99d2c52acf7d0015825973ad5ef6b1a07d
scypurple <scypurple@gmail.com>
2022-05-18 13:46:27 +0800

    codesandbox change

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,49 +1,54 @@
-import React, { Component } from 'react'
-import PubSub from 'pubsub-js'
+import React, { Component } from "react";
+import PubSub from "pubsub-js";
 
-import './index.css'
+import "./index.css";
 
 export default class List extends Component {
-
   state = {
     users: [],
     isFirst: true,
     isLoading: false,
-    error: '',
-  }
+    error: ""
+  };
 
   componentDidMount() {
-    this.token = PubSub.subscribe("updateState", (_, data) => this.setState(data));
+    this.token = PubSub.subscribe("updateState", (_, data) =>
+      this.setState(data)
+    );
   }
 
   componentWillUnmount() {
-    PubSub.unsubscribe(this.token)
+    PubSub.unsubscribe(this.token);
   }
 
   render() {
-    const {users, isFirst, isLoading, error} = this.state;
+    console.log("render");
+    const { users, isFirst, isLoading, error } = this.state;
     return (
       <div className="row">
-        {
-          isFirst ? <h1>Hi! lore!</h1> :
-          isLoading ? <h1>Loading...</h1> :
-          error ? <h1>{error}</h1> :
-          users.map(user => {
+        {isFirst ? (
+          <h1>Hi! lore!</h1>
+        ) : isLoading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <h1>{error}</h1>
+        ) : (
+          users.map((user) => {
             return (
-            <div className="card" key={user.id}>
-                <a href={user.html_url} target="_blank" rel='noreferrer'>
-                  <img alt="pic" style={{width: '100px'}} src={user.avatar_url} />
+              <div className="card" key={user.id}>
+                <a href={user.html_url} target="_blank" rel="noreferrer">
+                  <img
+                    alt="pic"
+                    style={{ width: "100px" }}
+                    src={user.avatar_url}
+                  />
                 </a>
                 <p className="card-text">{user.login}</p>
               </div>
-            )
+            );
           })
-        }
-              
-    
-        
-      
-    </div>
-    )
+        )}
+      </div>
+    );
   }
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,57 +1,61 @@
-import React, { Component } from 'react';
-//import axios from 'axios';
-import PubSub from 'pubsub-js'
-import './index.css';
+import React, { Component } from "react";
+import axios from "axios";
+import PubSub from "pubsub-js";
+import "./index.css";
 
 export default class Search extends Component {
-    keyWordContainer = React.createRef()
+  keyWordContainer = React.createRef();
 
-    search = async () => {
-        const { value } = this.keyWordContainer.current;
-        if (!value.trim()) return alert("输入不能为空");
-        PubSub.publish("updateState", { isFirst: false, isLoading: true })
-        // axios.get(`https://api.github.com/search/users?q=${value}`).then(
-        //     response => {
-        //    PubSub.publish("updateState", {users:response.data.items, isLoading: false})},
+  search = async () => {
+    const { value } = this.keyWordContainer.current;
+    if (!value.trim()) return alert("输入不能为空");
+    PubSub.publish("updateState", { isFirst: false, isLoading: true });
+    axios.get(`https://api.github.com/search/users?q=${value}`).then(
+      (response) => {
+        PubSub.publish("updateState", {
+          users: response.data.items,
+          isLoading: false
+        });
+      },
 
-        // error => { PubSub.publish("updateState", {error: error.message, isLoading: false})},
-        // )
-        // fetch(`https://api.github.com/search/users?q=${value}`).then(
-        //     response => {
-        //         return response.json()
-        //     }).then(
-        //     response => {
-        // PubSub.publish("updateState", {users:response.items, isLoading: false})
-        //     })
-        try {
-            const response = await fetch(`https://api.github.com/search/users?q=${value}`);
-            const data = await response.json();
-            //console.log(data.items);
-            PubSub.publish("updateState", { users: data.items, isLoading: false })
-        } catch (error) {
-            // error =>  PubSub.publish("updateState", {error: error.message, isLoading: false})
-            PubSub.publish("updateState", { error: error.message, isLoading: false })
-        }
+      (error) => {
+        PubSub.publish("updateState", {
+          error: error.message,
+          isLoading: false
+        });
+      }
+    );
+    // fetch(`https://api.github.com/search/users?q=${value}`).then(
+    //     response => {
+    //         return response.json()
+    //     }).then(
+    //     response => {
+    // PubSub.publish("updateState", {users:response.items, isLoading: false})
+    //     })
+  };
+
+  handleEnter = (e) => {
+    if (e.keyCode === 13) {
+      this.search();
     }
+  };
 
-    handleEnter = (e) => {
-        if (e.keyCode === 13) {
-            this.search();
-        }
-    }
-
-    render() {
-        return (
-            <section className="jumbotron">
-                <h3 className="jumbotron-heading">github用户搜索</h3>
-                <div>
-                    <input type="text"
-                        ref={this.keyWordContainer}
-                        placeholder="请输入搜索关键词"
-                        onKeyUp={this.handleEnter} />&nbsp;
-                    <button onClick={this.search}>搜索</button>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    console.log("searching");
+    return (
+      <section className="jumbotron">
+        <h3 className="jumbotron-heading">github用户搜索</h3>
+        <div>
+          <input
+            type="text"
+            ref={this.keyWordContainer}
+            placeholder="请输入搜索关键词"
+            onKeyUp={this.handleEnter}
+          />
+          &nbsp;
+          <button onClick={this.search}>搜索</button>
+        </div>
+      </section>
+    );
+  }
 }
